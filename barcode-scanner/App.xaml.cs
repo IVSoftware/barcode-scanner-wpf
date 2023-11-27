58c121e42457388b17f7db2ce5c5081e45c5053b
Tom Gregor <tgregor@ivsoftware.com>
2023-11-27 10:51:41 -0700

    Add project files.

---
+++ barcode-scanner/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace barcode_scanner
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ barcode-scanner/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace barcode_scanner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnLoad;
+        }
+
+        const int WM_CHAR = 0x0102;
+        private void OnLoad(object sender, RoutedEventArgs e)
+        {
+            _source = HwndSource.FromHwnd(new WindowInteropHelper(this).Handle);
+            _source.AddHook(WndProc);
+            Closed += (sender, e) =>
+            {
+                _source.RemoveHook(WndProc);
+            };
+        }
+        HwndSource? _source = null;
+
+        private nint WndProc(nint hwnd, int msg, nint wParam, nint lParam, ref bool handled)
+        {
+            if (msg.Equals(WM_CHAR)) detectScan((char)wParam);
+            return IntPtr.Zero;
+        }
+
+        private void detectScan(char @char)
+        {
+            if (_keyCount == 0) _buffer.Clear();
+            int charCountCapture = ++_keyCount;
+            _buffer.Append(@char);
+            Task
+                .Delay(TimeSpan.FromSeconds(SECONDS_PER_CHARACTER_MIN_PERIOD))
+                .GetAwaiter()
+                .OnCompleted(() =>
+                {
+                    if (charCountCapture.Equals(_keyCount))
+                    {
+                        _keyCount = 0;
+                        if (_buffer.Length > SCAN_MIN_LENGTH)
+                        {
+                            Application.Current.Dispatcher.Invoke(() =>
+                                MessageBox.Show(_buffer.ToString()));
+                        }
+                    }
+                });
+        }
+        private readonly StringBuilder _buffer = new StringBuilder();
+        int _keyCount = 0;
+        const int SCAN_MIN_LENGTH = 8;
+        const double SECONDS_PER_CHARACTER_MIN_PERIOD = 0.1;
+    }
+}
